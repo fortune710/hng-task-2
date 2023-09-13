89f1d1b6accb398a0185335c926ceca6e30e0a3d
fortune710 <fortune0208@yahoo.com>
2023-09-13 14:45:05 +0100

    added body parser for parsing json

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+
 const express = require('express');
+const MainRouter = require('./routes');
+
+const bodyParser = require('body-parser')
 const server = express();
+
+server.use(bodyParser.json())
+server.use('/api', MainRouter)
+
+
 
 
 server.listen(3000);
