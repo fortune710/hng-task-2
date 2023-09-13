39e800e6253901828a5ec63bbdc19bfd356c13e8
fortune710 <fortune0208@yahoo.com>
2023-09-13 14:44:21 +0100

    changed no name response from text to json

--- controllers/createPerson.js
+++ controllers/createPerson.js
@@ -1,13 +1,20 @@
 const { database } = require('../config/database.config');
-const { getDocs, setDoc, doc } = require('firebase/firestore');
+const { getDocs, setDoc, doc, collection } = require('firebase/firestore');
 
 module.exports.createPerson = async (req, res) => {
+    
     const { name, track } = req.body;
 
-    if(!name) return res.status(400).send('`name` property is missing from Request Body')
+
+    if(!name) {
+        return res.status(400).json({ message: '`name` property is missing from request body'})
+    }
 
     try {
-        const currentCount = (await getDocs()).docs.length;
+        const peopleCollectionRef = collection(database, "people")
+        const snapshot = await getDocs(peopleCollectionRef)
+        const currentCount = snapshot.docs.length;
+        
         const newPersonId = currentCount + 1;
 
         const newPersonRef = doc(database, "people", `${newPersonId}`);
@@ -17,7 +24,36 @@ module.exports.createPerson = async (req, res) => {
         };
 
         await setDoc(newPersonRef, newPerson)
-        return res.status(201).json({ newPerson })
+        return res.status(201).json({ newPerson: { id: newPersonId, ...newPerson } })
+    } catch (e) {
+        return res.status(500).json({ error: e, message: "Error while adding person" })
+    }
+
+}
+
+module.exports.createPersonFromParams = async (req, res) => {
+    
+    const { name } = req.params;
+
+
+    if(!name) {
+        return res.status(400).json({ message: '`name` property is missing from request body'})
+    }
+
+    try {
+        const peopleCollectionRef = collection(database, "people")
+        const snapshot = await getDocs(peopleCollectionRef)
+        const currentCount = snapshot.docs.length;
+        
+        const newPersonId = currentCount + 1;
+
+        const newPersonRef = doc(database, "people", `${newPersonId}`);
+        const newPerson = {
+            name, 
+        };
+
+        await setDoc(newPersonRef, newPerson)
+        return res.status(201).json({ newPerson: { id: newPersonId, ...newPerson } })
     } catch (e) {
         return res.status(500).json({ error: e, message: "Error while adding person" })
     }
